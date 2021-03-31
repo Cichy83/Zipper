aef3202640adaa2f0ee3a05d4ea3362cda4f29bd
bartoszp <bartoszpypec@poczta.onet.pl>
2021-04-01 01:54:41 +0200

    final version

--- src/FileZipper.java
+++ src/FileZipper.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 
 public class FileZipper extends JFrame {
@@ -59,6 +61,11 @@ public class FileZipper extends JFrame {
             return lista.get(index);
         }
 
+        public Object remove(int index) {
+            lista.remove(index);
+            return super.remove(index);
+        }
+
         ArrayList lista = new ArrayList();
 
     };
@@ -88,9 +95,9 @@ public class FileZipper extends JFrame {
             if (e.getActionCommand().equals("Dodaj"))
                 dodajWpisyDoArchiwum();
             else if (e.getActionCommand().equals("Usuń"))
-                System.out.println("Usuwanie");
+               UsuwanieWpisowZListy();
             else if (e.getActionCommand().equals("Zip"))
-                System.out.println("Zipowanie");
+                stworzArchiwumZip();
         }
 
         private void dodajWpisyDoArchiwum() {
@@ -120,8 +127,75 @@ public class FileZipper extends JFrame {
         }
         return false;
     }
+    private void UsuwanieWpisowZListy(){
+        int [] tmp = lista.getSelectedIndices();
+
+        for (int i = 0; i< tmp.length; i++){
+            System.out.println(tmp[i]);
+            modelListy.remove(tmp[i]-i);
+        }
+    }
+
+    private void stworzArchiwumZip(){
+        wybieracz.setCurrentDirectory(new File(System.getProperty("user.dir")));
+        wybieracz.setSelectedFile(new File(System.getProperty("user.dir")+File.separator+"mojanazwa.zip"));
+        int tmp = wybieracz.showDialog(rootPane, "Kompresuj");
+
+        if (tmp == JFileChooser.APPROVE_OPTION){
+            byte tmpData[] = new byte[BUFFOR];
+            try{
+                ZipOutputStream zOutS = new ZipOutputStream((new BufferedOutputStream(new FileOutputStream(wybieracz.getSelectedFile()))));
+                for (int i = 0; i < modelListy.getSize(); i++){
+                    if(!((File)(modelListy.get(i))).isDirectory()){
+                        zipuj(zOutS, (File)modelListy.get(i), tmpData, ((File)modelListy.get(i)).getPath());
+                    }
+                    else{
+                        wypiszSciezki((File)modelListy.get(i));
+                        for (int j = 0; j < listaSciezek.size(); j++){
+                            zipuj(zOutS, (File)listaSciezek.get(j), tmpData, ((File)modelListy.get(i)).getPath());
+                            listaSciezek.removeAll(listaSciezek);
+                        }
+                    }
+                }
+                zOutS.close();
+            }
+            catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        }
 
 
+
+
+    }
+    private void zipuj( ZipOutputStream zOutS, File sciezkaPLiku, byte[] tmpData, String sciezkaBazowa) throws IOException {
+        BufferedInputStream inS = new BufferedInputStream(new FileInputStream(sciezkaPLiku), BUFFOR);
+        zOutS.putNextEntry(new ZipEntry(sciezkaPLiku.getPath().substring(sciezkaBazowa.lastIndexOf(File.separator)+1)));
+        int counter;
+        while((counter = inS.read(tmpData, 0, BUFFOR)) != -1){
+            zOutS.write(tmpData,0,counter);
+        }
+        zOutS.closeEntry();
+        inS.close();
+    }
+
+
+    public static final int BUFFOR = 1024;
+
+    private void wypiszSciezki(File nazwaSciezki){
+        String[] nazwyPlikowIKatalogow = nazwaSciezki.list();
+        for (int i = 0; i < nazwyPlikowIKatalogow.length; i++){
+            File p = new File(nazwaSciezki.getPath(), nazwyPlikowIKatalogow[i]);
+            if (p.isFile()){
+                listaSciezek.add(p);
+            }
+            if (p.isDirectory()){
+                wypiszSciezki(new File(p.getPath()));
+            }
+        }
+    }
+
+    ArrayList listaSciezek = new ArrayList();
     public static void main(String[] args) {
         new FileZipper().setVisible(true);
     }
