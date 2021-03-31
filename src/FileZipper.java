d7d0c61160ef3c253feaa543059d7f42d82afd54
bartoszp <bartoszpypec@poczta.onet.pl>
2021-04-01 00:37:32 +0200

    Adding files to zipper

--- src/FileZipper.java
+++ src/FileZipper.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.sql.SQLOutput;
+import java.io.File;
+import java.util.ArrayList;
+
 
 public class FileZipper extends JFrame {
 
@@ -30,7 +32,7 @@ public class FileZipper extends JFrame {
 
     layout.setHorizontalGroup(
             layout.createSequentialGroup()
-            .addComponent(lista, 100, 150,Short.MAX_VALUE)
+            .addComponent(skrolek, 100, 150,Short.MAX_VALUE)
             .addContainerGap(0,Short.MAX_VALUE)
             .addGroup(
                     layout.createParallelGroup().addComponent(bDodaj).addComponent(bUsun).addComponent(bZip)
@@ -38,7 +40,7 @@ public class FileZipper extends JFrame {
     );
     layout.setVerticalGroup(
             layout.createParallelGroup()
-            .addComponent(lista, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(skrolek, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup().addComponent(bDodaj).addComponent(bUsun).addGap(5,40,Short.MAX_VALUE).addComponent(bZip))
     );
 
@@ -47,33 +49,76 @@ public class FileZipper extends JFrame {
     this.pack();
     }
 
-    private JList lista = new JList();
+    private DefaultListModel modelListy = new DefaultListModel(){
+        @Override
+        public void addElement(Object obj) {
+            lista.add(obj);
+            super.addElement(((File)obj).getName());
+        }
+        public Object get(int index) {
+            return lista.get(index);
+        }
+
+        ArrayList lista = new ArrayList();
+
+    };
+    private JList lista = new JList(modelListy);
     private JButton bDodaj;
     private JButton bUsun;
     private JButton bZip;
     private JMenuBar pasekMenu = new JMenuBar();
+    private JFileChooser wybieracz = new JFileChooser();
+    private JScrollPane skrolek = new JScrollPane(lista);
 
-    private class Akcja extends AbstractAction{
+    private class Akcja extends AbstractAction {
 
-        public Akcja(String nazwa, String opis, String klawiaturowySkrot){
+        public Akcja(String nazwa, String opis, String klawiaturowySkrot) {
             this.putValue(Action.NAME, nazwa);
             this.putValue(Action.SHORT_DESCRIPTION, opis);
             this.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(klawiaturowySkrot));
         }
-        public Akcja(String nazwa, String opis, String klawiaturowySkrot, Icon ikona){
+
+        public Akcja(String nazwa, String opis, String klawiaturowySkrot, Icon ikona) {
             this(nazwa, opis, klawiaturowySkrot);
-            this.putValue(Action.SMALL_ICON,ikona);
+            this.putValue(Action.SMALL_ICON, ikona);
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
             if (e.getActionCommand().equals("Dodaj"))
-                System.out.println("Dodawanie");
+                dodajWpisyDoArchiwum();
             else if (e.getActionCommand().equals("Usuń"))
                 System.out.println("Usuwanie");
             else if (e.getActionCommand().equals("Zip"))
                 System.out.println("Zipowanie");
         }
+
+        private void dodajWpisyDoArchiwum() {
+            wybieracz.setCurrentDirectory(new File(System.getProperty("user.dir")));
+            wybieracz.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+            wybieracz.setMultiSelectionEnabled(true);
+
+            int tmp = wybieracz.showDialog(rootPane, "Dodaj do archiwum");
+
+            if (tmp == JFileChooser.APPROVE_OPTION) {
+                File[] sciezki = wybieracz.getSelectedFiles();
+
+                for (int i = 0; i < sciezki.length; i++) {
+                    if(!czyWpisSiePowtarza(sciezki[i].getPath())) {
+                        modelListy.addElement(sciezki[i]);
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean czyWpisSiePowtarza(String testowanyWpis){
+        for (int i = 0; i < modelListy.getSize(); i++){
+            if ( ((File)modelListy.get(i)).getPath().equals(testowanyWpis)){
+                return true;
+            }
+        }
+        return false;
     }
 
 
